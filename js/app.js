801a5f92c9c7a9326e9bbda20eaf5eebf9025c54
viveloper <usik1234@gmail.com>
2020-02-15 23:34:48 +0900

    smooth scroll

--- js/app.js
+++ js/app.js
@@ -17,3 +17,32 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('fixed');
   }
 });
+
+// **************** smooth scroll *****************
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    navLinks.classList.remove('show-links');
+    const id = e.target.getAttribute('href');
+    const element = document.querySelector(id);
+    let position;
+    if (navbar.classList.contains('fixed')) {
+      position = element.offsetTop - 62;
+    } else {
+      position = element.offsetTop - 124;
+    }
+    if (window.innerWidth < 992) {
+      if (navbar.classList.contains('fixed')) {
+        position = element.offsetTop - 62;
+      } else {
+        position = element.offsetTop - 270 - 124;
+      }
+    }
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: 'smooth'
+    });
+  });
+});
